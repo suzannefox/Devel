6b3a4d030935dbbc0c30280e713182d51db4553a
Suzanne Fox <Suzanne_Fox@hotmail.com>
2017-02-07 14:26:05 +0000

    Final Structure, 2017 02 07 #4

--- Plot_Categories.R
+++ Plot_Categories.R
@@ -2,9 +2,12 @@
 workdir <- "C:/Users/Suzanne/OneDrive - Suzanne Fox/Dissertation/R/"
 source("C:/Users/Suzanne/OneDrive - Suzanne Fox/Dissertation/R/Burrow.R")
 
-Data.choose <- "Hair"
+#Data.choose = "x"
+#Data.choose <- "Hair"
 #Data.choose <- "iris"
 #Data.choose <- "mtcars"
+Data.choose <- "Arth"
+
 
 if (Data.choose=="iris") {
   Data.source <- iris
@@ -17,6 +20,11 @@ if (Data.choose=="iris") {
 } else if (Data.choose=="Hair") {
   Data.source <- as.data.frame(HairEyeColor)
   Data.title <- "HairEyeColor"
+  
+} else if (Data.choose=="Arth") {
+  library(vcd)
+  Data.source <- as.data.frame(Arthritis)
+  Data.title <- "Arthritis"
   
 } else {
   Data.source <- read.csv(paste(workdir,"TestData.csv",sep=""))
@@ -41,27 +49,32 @@ if (nrow(Data.vars.category)==0) {
 } else {
   Data.list.categories <- as.list(Data.vars.category[c("Variable1")])
   Data.categorical <- as.data.frame(Data.source[,c(Data.list.categories$Variable1)])
+  names(Data.categorical) <- Data.list.categories$Variable1
   
-  table(Data.categorical)
+  for (i in 1:nrow(Data.vars.category)) {
+    # Generate data
+    Data.col <- Data.vars.category[i,c("Variable1")]
+
+    x1 <- as.data.frame(table(Data.categorical[,c(Data.col)]), stringsAsFactors = FALSE)
+    sumx1 <- sum(x1$Freq)
+    
+    names(x1) <- c(Data.col,"Count")
+    x1$Pcent <- x1$Count / sumx1 * 100
+    
+    myPlot <- ggplot(data=x1, 
+           aes_string(x=Data.col,  y="Pcent")) +
+      geom_bar(colour="black", fill="#4C1E9C",
+               width=.8, stat="identity") +
+      xlab("Categories") + ylab("% of Total Sample") +
+      ggtitle(paste("Distribution of Categories for Variable :",Data.col)) +
+      theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))
+    
+    print(myPlot)
+  }
 
 }
 
 # ==================================
 
-Data.col <- "Sex"
-x1 <- as.data.frame(table(Data.categorical[,c(Data.col)]), stringsAsFactors = FALSE)
-sumx1 <- sum(x1$Freq)
-
-#x1 <- rbind(data.frame(Var1=c("TOTAL"), Freq=sumx1), x1)
-names(x1) <- c(Data.col,"Count")
-x1$Pcent <- x1$Count / sumx1 * 100
-
-ggplot(data=x1, 
-       aes_string(x=Data.col,  y="Pcent")) +
-  geom_bar(colour="black", fill="#4C1E9C",
-           width=.8, stat="identity") +
-  xlab("Variables") + ylab("% of Total Sample") +
-  ggtitle(paste("Distribution of Categories for Variable :",Data.col)) +
-  theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))
 
 
